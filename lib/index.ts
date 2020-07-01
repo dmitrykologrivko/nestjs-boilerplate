1625762fab9bb0d07fb3fe2b2d5c855d743d8384
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2020-07-01 20:42:05 +0300

    Library structure improvements

---
+++ lib/index.ts
@@ -0,0 +1,10 @@
+export * from './bootstrap';
+export * from './config';
+export * from './database';
+export * as domain from './domain';
+export * from './environment';
+export * from './management';
+export * from './rest';
+export * from './server';
+export * from './utils';
+export * from './core.module';
