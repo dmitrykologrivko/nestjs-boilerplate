c656faa05c20d2d3d1ad41e8b7199823ecb0ad46
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2023-01-24 23:24:05 +0300

    Request factory function is added to AuthorizedUser and BearerToken decorators

--- packages/auth/src/decorators/authorized-user.decorator.ts
+++ packages/auth/src/decorators/authorized-user.decorator.ts
@@ -1,8 +1,13 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request, fromExpressRequest } from '@nestjs-boilerplate/core';
 
-export const AuthorizedUser = createParamDecorator(
-    (data: unknown, ctx: ExecutionContext) => {
-        const request = ctx.switchToHttp().getRequest();
+export const AuthorizedUser = (
+    requestFactory: (req: any) => Request = fromExpressRequest,
+): ParameterDecorator => {
+    return createParamDecorator((data: any, ctx: ExecutionContext) => {
+        const request = data.requestFactory(
+            ctx.switchToHttp().getRequest(),
+        );
         return request.user;
-    },
-);
+    })({ requestFactory });
+};
--- packages/auth/src/decorators/bearer-token.decorator.ts
+++ packages/auth/src/decorators/bearer-token.decorator.ts
@@ -1,15 +1,20 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request, fromExpressRequest } from '@nestjs-boilerplate/core';
 
 const AUTHORIZATION_HEADER = 'authorization';
 
-export const BearerToken = createParamDecorator(
-    (data: unknown, ctx: ExecutionContext) => {
-        const request = ctx.switchToHttp().getRequest();
+export const BearerToken = (
+    requestFactory: (req: any) => Request = fromExpressRequest,
+): ParameterDecorator => {
+    return createParamDecorator((data: any, ctx: ExecutionContext) => {
+        const request = data.requestFactory(
+            ctx.switchToHttp().getRequest(),
+        );
 
         if (!request.headers.hasOwnProperty(AUTHORIZATION_HEADER)) {
             return null;
         }
 
         return request.headers[AUTHORIZATION_HEADER].replace(/^Bearer\s/, '');
-    },
-);
+    })({ requestFactory });
+};
