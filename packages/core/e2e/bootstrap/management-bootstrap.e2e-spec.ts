d52a51176c8d6b9292910527d5efb431d91c3912
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2025-04-08 23:39:17 +0300

    Fix skipped test

--- packages/core/e2e/bootstrap/management-bootstrap.e2e-spec.ts
+++ packages/core/e2e/bootstrap/management-bootstrap.e2e-spec.ts
@@ -1,11 +1,9 @@
 import { execSync } from 'child_process';
 
 describe('Management Bootstrap', () => {
-    it.skip('should command run successfully', async () => {
+    it('should command run successfully', async () => {
         const name = `John-${Math.random()}}`;
-        const command = `
-            ./node_modules/.bin/ts-node ./e2e/bootstrap/management-src/main.ts --command greetings --name ${name}
-        `;
+        const command = `./node_modules/.bin/ts-node ./packages/core/e2e/bootstrap/management-src/main.ts --command greetings --name ${name}`;
 
         const output = execSync(command);
         expect(output.toString()).toContain(`Hello ${name}!`);
